6d9847b0c2fd3478f1c29c7adab65b8cd5919e98
jnr <13692453351@163.com>
2020-09-17 19:42:07 +0800

    第一次提交项目@JNR

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+//引入 layui css
+//引入 layui css
+import '../node_modules/layui-src/dist/css/layui.css'
+import '../node_modules/layui-src/dist/layui.js'
 
 Vue.config.productionTip = false
 
